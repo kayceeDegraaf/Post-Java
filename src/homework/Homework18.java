b4e620720c4ce3e09825eed39ef313f61668474e
kayceeDegraaf <katelyndegraaf@gmail.com>
2022-05-10 14:20:56 -0500

    second push

--- src/homework/Homework18.java
+++ src/homework/Homework18.java
@@ -3,11 +3,22 @@ package homework;
 public class Homework18 {
     public static void main(String[] args) {
         //Test Here
-        System.out.println(noSpace("Hello I am Kaycee"));
-        System.out.println(replaceFirstLast("   K          "));
+        System.out.println("\n------METHOD 1------");
+        System.out.println(noSpace1("Hello      I am Kaycee"));
+        System.out.println(noSpace2("Hello      I am Kaycee"));
+        System.out.println(noSpace3("Hello      I am Kaycee"));
+        System.out.println("\n------METHOD 2------");
+        System.out.println(replaceFirstLast(""));
+        System.out.println(replaceFirstLast("A"));
+        System.out.println(replaceFirstLast("   A   "));
+        System.out.println(replaceFirstLast("Hello"));
+        System.out.println(replaceFirstLast("Tech Global"));
+        System.out.println("\n------METHOD 3------");
+        System.out.println(hasVowel(""));
+        System.out.println(hasVowel("Java"));
+        System.out.println(hasVowel("1234"));
+        System.out.println(hasVowel("ABC"));
     }
-
-
 
 
 
@@ -20,9 +31,26 @@ public class Homework18 {
      with all spaces removed from the original String
      */
 
-    public static String noSpace(String str){
+    public static String noSpace1(String str){
         return str.replaceAll("\\s", "");
     }
+
+    public static String noSpace2(String str){
+        String result = "";
+        for (int i = 0; i <str.length(); i++){
+            if (!Character.isWhitespace(str.charAt(i))) result += str.charAt(i);
+        }
+        return result;
+    }
+
+    public static String noSpace3(String str){
+        String result = "";
+        for (char element : str.toCharArray()){
+            if (element != ' ') result += element;
+        }
+        return result;
+    }
+
 
     //Method 2
     /*
@@ -34,9 +62,47 @@ public class Homework18 {
     NOTE: Ignore all before and after spaces (get actual String only)
      */
     public static String replaceFirstLast(String str){
-
-        if (str.length() < 2) return "";
-        return str.substring(str.length()-1) + str.substring(1, str.length()-1)
-                + str.substring(0,1);
+        if (str.trim().length() < 2) return "";
+        return str.substring(str.length()-1) + str.substring(1, str.length()-1) + str.substring(0,1);
     }
+
+    //Method 3
+    /*
+    Requirement:
+    -Create a method called hasVowel()
+    -This method will take one String argument, and it will return a boolean
+     checking if String has any vowel or not
+    NOTE: Vowels are = a, e, o, u, I
+    NOTE: Ignore cases
+
+     */
+
+    public static boolean hasVowel(String str){
+        boolean vowel = false;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.toLowerCase().charAt(i) == 'a' || str.toLowerCase().charAt(i) == 'e' ||
+                    str.toLowerCase().charAt(i) == 'i' || str.toLowerCase().charAt(i) == 'o'
+            || str.toLowerCase().charAt(i) == 'u') vowel = true;
+        }
+        return vowel;
+    }
+
+    //METHOD 4
+    /*
+    Requirement:
+    -Create a method called checkAge()
+    -This method will take an int yearOfBirth as  argument, and it will print message below based on the entry
+    If the age is less than 16, then print “AGE IS NOT ALLOWED”
+    If the age is 16 or more, then print “AGE IS ALLOWED”
+    If the age is more than 100 or a future year entered, print “AGE IS NOT VALID”
+    NOTE: Calculate the age taking base year as current year in a dynamic way. You can use Date class.
+     */
+
+    public static void checkAge(int yearOfBirth){
+
+    }
+
+
+
+
 }
