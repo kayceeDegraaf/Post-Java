76c03f95905f299a4456e4bb3f4e020268be1509
kayceeDegraaf <katelyndegraaf@gmail.com>
2022-06-20 11:51:49 -0500

    homework22

--- src/homework/Homework22.java
+++ src/homework/Homework22.java
@@ -3,6 +3,16 @@ package homework;
 import java.util.*;
 
 public class Homework22 {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {3, 4, 5, 5};
+        int[] arr3 = {1,2,2,3};
+        System.out.println(Arrays.toString(fibonacciSeries1(7)));
+        System.out.println(fibonacciSeries2(8));
+        System.out.println(Arrays.toString(findUniques(arr1, arr2)));
+        System.out.println(isPowerOf3(81));
+        System.out.println(firstDuplicate(arr3));
+    }
     //Task-1
     public static int[] fibonacciSeries1(int num) {
         int[] arr = new int[num];
@@ -54,16 +64,7 @@ public class Homework22 {
     }
 
 
-    public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4};
-        int[] arr2 = {3, 4, 5, 5};
-        int[] arr3 = {1,2,2,3};
-        System.out.println(Arrays.toString(fibonacciSeries1(7)));
-        System.out.println(fibonacciSeries2(8));
-        System.out.println(Arrays.toString(findUniques(arr1, arr2)));
-        System.out.println(isPowerOf3(81));
-        System.out.println(firstDuplicate(arr3));
-    }
+
 
 
 }
